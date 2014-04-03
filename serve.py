f93c420254d5193cd106c0204f8ea630079013c6
Lynn Cyrin <firemagelynn@gmail.com>
2014-04-03 01:08:04 -0700

    i dont like databases

--- serve.py
+++ serve.py
@@ -19,41 +19,6 @@ app.config.update(dict(
 #doesn't currently exist
 app.config.from_envvar('BLOG_SETTINGS', silent=True) 
 
-#database shenanigans
-def init_database ():
-    with app.app_context():
-        #create an application context
-        database = get_database()
-        with app.open_resource('schema.sql', mode='r') as f: #as file
-            database.cursor().executescript(f.read())
-        database.commit()
-
-def connect_database ():
-    #what is rv??? unsure
-    rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlite3.Row
-    return rv
-
-def get_database ():
-    '''
-    opens a new database connection if there isn't one for the current
-    application context
-
-    flask.g = general application context variable
-    '''
-    #if you haven't gotten the database and attached it to the g
-    if not hasattr(flask.g, 'sqlite_database'):
-        #then connect to it
-        flask.g.sqlite_database = connect_database()
-    #and return it?
-    return flask.g.sqlite_database
-
-@app.teardown_appcontext
-def close_database (error):
-    '''turns off the database connection when we are done with it'''
-    if hasattr (flask.g, 'sqlite_database'):
-        flask.g.sqlite_database.close()
-
 #views
 @app.route('/')
 def index ():
