584b0dc74ddaf8aaf766771ee07f00a14edf6f8b
Lynn Cyrin <firemagelynn@gmail.com>
2014-04-05 12:10:32 -0700

    enable offline resources

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 #config.py
 #var names all caps!
+#boolean: True | False
 class config (object):
-	DEBUG = True
-	SITENAME = "a lynn blog"
-	AVATAR = "http://www.gravatar.com/avatar/b1cde28ce033c8cd6f4be4059efbe00b.png?size=160"
-	URL = "http://lynncyrin.me"
-	DESC = "@me Queer, Feminist, Programmer"
+    DEBUG = True
+    OFFLINE = True
+    SITENAME = "a lynn blog"
+    AVATAR = "http://www.gravatar.com/avatar/b1cde28ce033c8cd6f4be4059efbe00b.png?size=160"
+    URL = "http://lynncyrin.me"
+    DESC = "Queer, Feminist, Programmer"
