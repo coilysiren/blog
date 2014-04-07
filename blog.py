0c6769b215ad7cbc4397a5a879d659d3ad317c68
Lynn Cyrin <firemagelynn@gmail.com>
2014-04-06 23:57:22 -0700

    404 is now a page

--- blog.py
+++ blog.py
@@ -30,20 +30,20 @@ def contact (): return flask.render_template('post.html', post_url='pages/contac
 @app.route('/name')
 def name (): return flask.render_template('post.html', post_url='pages/name.html')
 
+@app.errorhandler(404)
+def page_not_found (e): return flask.render_template('post.html', post_url='pages/404.html'), 404
+
 @app.route('/post/<post_title>')
 def show_post_by_title (post_title):
 	post_title = post_title.lower() #clean input
 	file_built = build_html(post_title) #build your html file
 	if file_built: post_url = 'rendered_posts/'+str(post_title)+'.html'
-	else: return flask.render_template('404.html'), 404 #no such file exists
+	else: return page_not_found(404) #no such file exists
 	return flask.render_template('post.html', post_url=post_url)
 
 @app.route('/recent/<post_number>')
 def show_post_by_recentness (post_number):
 	return "WIP"
-
-@app.errorhandler(404)
-def page_not_found (e): return flask.render_template('404.html'), 404
 
 #functions
 
