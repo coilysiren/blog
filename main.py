f839db3ac2b0c80d3b5edf4081e2bb9233f8380b
Lynn 'Cyrin' Conway <lynncyrin@gmail.com>
2014-07-14 05:21:24 -0700

    shuffled some code around
    everything still looks the same though
    
    i promise
    
    hopefully?

--- main.py
+++ main.py
@@ -10,12 +10,10 @@ Use:
 
 
 #builtin scripts
-import os
 import glob
 #external scripts
 import yaml
 import flask
-import stripe
 import flask.ext.scss
 #custom scripts
 import cms
@@ -24,15 +22,7 @@ import cms
 #start app and set configuration
 
 
-# stripe
-stripe_keys = {
-    'secret_key': os.environ['SECRET_KEY'],
-    'publishable_key': os.environ['PUBLISHABLE_KEY']
-}
-stripe.api_key = stripe_keys['secret_key']
-# init app
 app = flask.Flask(__name__, static_folder='static', static_url_path='')
-# my configs
 app.config.from_object(__name__)
 for key, value in yaml.load(file('config.yaml','r')).items():
     app.config[key] = value
@@ -43,47 +33,32 @@ for key, value in yaml.load(file('config.yaml','r')).items():
 
 #index page
 @app.route('/index')
-@app.route('/home')
 @app.route('/')
 def index ():
-    #need to get rid of the page_title + page_desc repitition
-    page_title = app.config['SITENAME']
-    page_desc = app.config['DESC']
-    #things to display on the landing page
-    post_urls = list()
-    post_urls.append('pages/landing.html')
-    post_urls.append('posts/origin-story_snipped.html')
-    post_urls.append('posts/intern-problems_snipped.html')
-    post_urls.append('posts/health-tracker_snipped.html')
-    post_urls.append('pages/about.html')
-    #dont edit return line
-    return flask.render_template('post.html', page_title=page_title, page_desc=page_desc, post_urls=post_urls)
+    return flask.render_template('post.html',
+        page_title=app.config['SITENAME'],
+        page_desc=app.config['DESC'],
+        post_urls=['pages/landing.html',
+            'posts/origin-story_snipped.html',
+            'posts/intern-problems_snipped.html',
+            'posts/health-tracker_snipped.html',
+            'pages/about.html'])
 
 #about page
-@app.route('/aboutme')
 @app.route('/about')
 def about ():
-    page_title = app.config['SITENAME']+' // About Me'
-    page_desc = app.config['DESC']+' // Information about me'
-    post_urls = ['pages/about.html', 'pages/contact.html', 'pages/name.html']
-    #dont edit return line
-    return flask.render_template('post.html', page_title=page_title, page_desc=page_desc, post_urls=post_urls)
+    return flask.render_template('post.html',
+        page_title=app.config['SITENAME']+' // About Me',
+        page_desc=app.config['DESC']+' // Information about me',
+        post_urls=['pages/about.html', 'pages/contact.html', 'pages/name.html'])
 
 #contact page
 @app.route('/contact')
 def contact ():
-    page_title = app.config['SITENAME']+' // Contact'
-    page_desc = app.config['DESC']+' // Contact information and links'
-    post_urls = ['pages/contact_guide.html', 'pages/contact.html']
-    #dont edit return line
-    return flask.render_template('post.html', page_title=page_title, page_desc=page_desc, post_urls=post_urls)
-
-@app.errorhandler(404)
-def page_not_found (e):
-    page_title = app.config['SITENAME']+' // Error 404'
-    page_desc = 'Page Not Found'
-    post_urls = ['pages/404.html']
-    return flask.render_template('post.html', page_title=page_title, page_desc=page_desc, post_urls=post_urls), 404
+    return flask.render_template('post.html',
+        page_title=app.config['SITENAME']+' // Contact',
+        page_desc=app.config['DESC']+' // Contact information and links',
+        post_urls=['pages/contact_guide.html', 'pages/contact.html'])
 
 @app.route('/posts/<post_title>')
 @app.route('/post/<post_title>')
@@ -91,12 +66,13 @@ def show_post_by_title (post_title):
     post_title = post_title.lower() #clean input
     try:
         with open('templates/posts/'+post_title+'.html'): pass
-    except IOError: return page_not_found(404)
+    except IOError:
+        return page_not_found(404)
     meta = yaml.load(file('templates/posts/'+post_title+'_meta.yaml','r'))
-    page_title = app.config['SITENAME']+' // '+meta['title']
-    page_desc = meta['desc']
-    post_urls = ['posts/'+post_title+'.html']
-    return flask.render_template('post.html', page_title=page_title, page_desc=page_desc, post_urls=post_urls)
+    return flask.render_template('post.html',
+        page_title=app.config['SITENAME']+' // '+meta['title'],
+        page_desc=meta['desc'],
+        post_urls=['posts/'+post_title+'.html'])
 
 @app.route('/static/<path:filename>')
 def base_static(filename):
@@ -104,14 +80,25 @@ def base_static(filename):
 
 @app.route('/tagged/<tag>')
 def tagged_page (tag):
-    page_title = app.config['SITENAME']+' // '+tag
-    page_desc = 'Posts tagged '+tag
     post_urls = list()
     for post in glob.glob('posts/*'):
         meta = yaml.load(file('templates/'+post[:-3]+'_meta.yaml','r'))
         if tag in meta['tags']:
             post_urls.append((post[:-3]+"_snipped.html"))
-    return flask.render_template('post.html', page_title=page_title, page_desc=page_desc, post_urls=post_urls)
+    return flask.render_template('post.html',
+        page_title=app.config['SITENAME']+' // '+tag,
+        page_desc='Posts tagged '+tag,
+        post_urls=post_urls)
+
+##########################
+#### Should be a form ####
+##########################
+@app.errorhandler(404)
+def page_not_found (e):
+    return flask.render_template('post.html',
+        page_title=app.config['SITENAME']+' // Error 404',
+        page_desc='Page Not Found',
+        post_urls=['pages/404.html'])
 
 
 #debug mode start options
