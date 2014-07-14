fe1376469ea1836cd465e6d93867a572a1d274c2
Lynn 'Cyrin' Conway <lynncyrin@gmail.com>
2014-07-14 05:22:37 -0700

    deleted a note

--- main.py
+++ main.py
@@ -90,9 +90,6 @@ def tagged_page (tag):
         page_desc='Posts tagged '+tag,
         post_urls=post_urls)
 
-##########################
-#### Should be a form ####
-##########################
 @app.errorhandler(404)
 def page_not_found (e):
     return flask.render_template('post.html',
